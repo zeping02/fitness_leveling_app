77cc454ba4d1aca36f97fb284bd1cd24ff9df313
zeping02_Thuis <zeping02@gmail.com>
2025-04-26 20:20:24 +0200

    initial commit

---
+++ scripts/Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	pass # Replace with function body.
